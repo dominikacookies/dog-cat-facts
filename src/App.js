44fb20f3d947c9c686883b2a9dfc4248a6e22364
Dominika <domi_nika@hotmail.co.uk>
2021-07-06 13:59:05 +0100

    captures which animal was selected in state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import PreferenceForm from "./components/PreferenceForm";
 class App extends Component {
   constructor(props) {
     super (props);
+
+    this.state = {
+      animal: "dog"
+    }
+  }
+
+  selectAnimal = (event) => {
+    this.setState({
+      animal: event.target.id
+    })
   }
 
   render() {
@@ -13,7 +23,7 @@ class App extends Component {
       <section className="main-container">
         <div className="content">
           <h1> Hello </h1> 
-          <PreferenceForm />
+          <PreferenceForm selectAnimal={this.selectAnimal}/>
         </div>
     </section>
     )
--- src/components/PreferenceForm.js
+++ src/components/PreferenceForm.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-const PreferenceForm = () => {
+const PreferenceForm = (props) => {
   return (
     <form>
       <div className="input-group mb-3">
         <input type="input" className="form-control" placeholder="Name" aria-label="Example text with two button addons"/>
-        <button className="btn btn-outline-secondary" type="button">Dogs</button>
-        <button className="btn btn-outline-secondary" type="button">Cats</button>      
+        <button className="btn btn-outline-secondary" type="button" id="dog" onClick={props.selectAnimal}>Dogs</button>
+        <button className="btn btn-outline-secondary" type="button"id="cat" onClick={props.selectAnimal}>Cats</button>      
       </div>
       <button type="submit" className="btn btn-primary">
         Get facts!
