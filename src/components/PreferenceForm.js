470147fd51fb4fdf8df1b533ea5a4f07a99cc071
Dominika <domi_nika@hotmail.co.uk>
2021-07-06 14:22:15 +0100

    updated placeholder name input and submit button colour

--- src/components/PreferenceForm.js
+++ src/components/PreferenceForm.js
@@ -4,11 +4,11 @@ const PreferenceForm = (props) => {
   return (
     <form onSubmit={props.onSubmit}>
       <div className="input-group mb-3">
-        <input type="input" className="form-control" placeholder="Name" aria-label="Example text with two button addons"/>
+        <input type="input" className="form-control" placeholder="Your name" aria-label="Example text with two button addons"/>
         <button className="btn btn-success" type="button" id="dog" onClick={props.selectAnimal}>Dogs</button>
         <button className="btn btn-outline-success" type="button" id="cat" onClick={props.selectAnimal}>Cats</button>      
       </div>
-      <button type="submit" className="btn btn-primary">
+      <button type="submit" className="btn btn-success">
         Get facts!
       </button>
     </form> 
