c94a5495bb5e2c92e726f0ac4a75f3cd1f5f81ae
Dominika <domi_nika@hotmail.co.uk>
2021-07-06 19:46:15 +0100

    added cors anyhere to fetch dog facts

--- src/utils/fetchFact.js
+++ src/utils/fetchFact.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const fetchCatFact = async () => {
   try {
     const { data } = await axios.get("https://catfact.ninja/fact");
-
     return data.fact;
+
   } catch (error) {
     return { error: "Failed to fetch data." };
   }
@@ -12,9 +12,10 @@ const fetchCatFact = async () => {
 
 const fetchDogFact = async () => {
   try {
-    const { data } = await axios.get("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1",  { crossdomain: true });
-
-    return data;
+    const { data } = await axios.get("https://cors-anywhere.herokuapp.com/https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1",
+     {headers: {"Access-Control-Allow-Origin" : "*", 'Access-Control-Allow-Credentials':true}});
+     return data[0].fact;
+     
   } catch (error) {
     return { error: "Failed to fetch data." };
   }
