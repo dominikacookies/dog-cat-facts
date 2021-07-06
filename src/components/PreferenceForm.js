23540c9f831731b2564ce099fdf7efc7392d1203
Dominika <domi_nika@hotmail.co.uk>
2021-07-06 13:32:14 +0100

    changed form style for better UX

--- src/components/PreferenceForm.js
+++ src/components/PreferenceForm.js
@@ -3,20 +3,10 @@ import React from "react"
 const PreferenceForm = () => {
   return (
     <form>
-      <div className="mb-3">
-        <div>
-          <input
-            type="input"
-            className="form-control"
-            id="name"
-            placeholder="Name"
-          />
-        </div>
-      </div>
-      <div className="form-check form-switch">
-        <span> Cats </span>
-          <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
-        <span> Dogs </span>
+      <div className="input-group mb-3">
+        <input type="input" className="form-control" placeholder="Name" aria-label="Example text with two button addons"/>
+        <button className="btn btn-outline-secondary" type="button">Dogs</button>
+        <button className="btn btn-outline-secondary" type="button">Cats</button>      
       </div>
       <button type="submit" className="btn btn-primary">
         Get facts!
